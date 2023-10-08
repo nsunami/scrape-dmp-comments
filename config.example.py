abbadc8da4b81aa12ef0f1310de45ff4b5b773c0
Nami Sunami <nsunami@pm.me>
2023-10-08 21:45:14 +0200

    adding example config file

---
+++ config.example.py
@@ -0,0 +1,9 @@
+# Set cookies and headers for handling log in
+## For getting the cookies and headers, see https://curlconverter.com/python/
+cookies = {
+    # Your cookies
+}
+
+headers = {
+    # Your headers
+}
